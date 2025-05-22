0339b51c44b65ca7c73a7b42f28f3c94475e8479
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-05-21 21:51:32 -0700

    Collapsed gapped and ungapped code into a single function.
    Fleshed out tests for max_gap and min_overlap cases.

--- include/nclist/overlaps_any.hpp
+++ include/nclist/overlaps_any.hpp
@@ -15,150 +15,16 @@ struct OverlapsAnyWorkspace {
     /**
      * @cond
      */
-    std::vector<std::pair<Index_, Index_> > history;
+    struct State {
+        State() = default;
+        State(Index_ cat, Index_ cend) : child_at(cat), child_end(cend) {}
+        Index_ child_at = 0, child_end = 0;
+    };
+    std::vector<State> history;
     /**
      * @endcond
      */
 };
-
-/**
- * @cond
- */
-template<typename Index_, typename Position_>
-void overlaps_any_simple(
-    const Nclist<Index_, Position_>& index,
-    Position_ query_start,
-    Position_ query_end,
-    Position_ min_overlap,
-    bool quit_on_first,
-    OverlapsAnyWorkspace<Index_>& workspace,
-    std::vector<Index_>& matches)
-{
-    matches.clear();
-    if (min_overlap > 0 && query_end - query_start < min_overlap) {
-        return;
-    }
-
-    // We use an upper bound as the interval ends are assumed to be non-inclusive,
-    // so we want the first end that is greater than the query_start.
-    Index_ root_progress = std::upper_bound(index.ends.begin(), index.ends.begin() + index.root_children, query_start) - index.ends.begin();
-    workspace.history.clear();
-
-    while (1) {
-        Index_ current_index;
-        if (workspace.history.empty()) {
-            if (root_progress == index.root_children || index.starts[root_progress] >= query_end) {
-                break;
-            }
-            current_index = root_progress;
-            ++root_progress;
-        } else {
-            auto& current_state = workspace.history.back();
-            if (current_state.second == index.nodes[current_state.first].children_end || index.starts[current_state.second] >= query_end) {
-                workspace.history.pop_back();
-                continue;
-            }
-            current_index = current_state.second;
-            ++(current_state.second); // do this before the emplace_back(), otherwise the history might get reallocated and the reference would be dangling.
-        }
-
-        const auto& current_node = index.nodes[current_index];
-        if (min_overlap > 0) {
-            if (std::min(query_end, index.ends[current_index]) - std::max(query_start, index.starts[current_index]) < min_overlap) {
-                // No point continuing with the children, as all children will by definition have smaller overlaps and cannot satisfy 'min_overlap'.
-                continue;
-            }
-        }
-
-        matches.push_back(current_node.id);
-        if (quit_on_first) {
-            return;
-        }
-        if (current_node.duplicates_start != current_node.duplicates_end) {
-            matches.insert(matches.end(), index.duplicates.begin() + current_node.duplicates_start, index.duplicates.begin() + current_node.duplicates_end);
-        }
-
-        if (current_node.children_start != current_node.children_end) {
-            Index_ start_pos = std::upper_bound(index.ends.begin() + current_node.children_start, index.ends.begin() + current_node.children_end, query_start) - index.ends.begin();
-            if (start_pos != current_node.children_end) {
-                workspace.history.emplace_back(current_index, start_pos);
-            }
-        }
-    }
-}
-
-template<typename Index_, typename Position_>
-void overlaps_any_gapped(
-    const Nclist<Index_, Position_>& index,
-    Position_ query_start,
-    Position_ query_end,
-    Position_ max_gap,
-    bool quit_on_first,
-    OverlapsAnyWorkspace<Index_>& workspace,
-    std::vector<Index_>& matches)
-{
-    auto is_finished = [&](Position_ index_start) -> bool {
-        if (index_start < query_end) {
-            return false;
-        }
-        return index_start - query_end > max_gap;
-    };
-
-    auto search_query_start = query_start;
-    if (std::is_unsigned<Position_>::value && search_query_start < max_gap) {
-        search_query_start = 0;
-    } else {
-        search_query_start -= max_gap;
-    }
-
-    // Now use a lower bound as the gap-subtracted start is inclusive with the interval ends.
-    // Consider a gap of zero; this means that a query that is exactly contiguous with a 
-    // subject interval will be considered as overlapping that interval.
-    Index_ root_progress = std::lower_bound(index.ends.begin(), index.ends.begin() + index.root_children, search_query_start) - index.ends.begin();
-    workspace.history.clear();
-    matches.clear();
-
-    while (1) {
-        Index_ current_index;
-        if (workspace.history.empty()) {
-            if (root_progress == index.root_children || is_finished(index.starts[root_progress])) {
-                break;
-            }
-            current_index = root_progress;
-            ++root_progress;
-        } else {
-            auto& current_state = workspace.history.back();
-            if (current_state.second == index.nodes[current_state.first].children_end || is_finished(index.starts[current_state.second])) {
-                workspace.history.pop_back();
-                continue;
-            }
-            current_index = current_state.second;
-            ++(current_state.second); // do this before the emplace_back(), otherwise the history might get reallocated and the reference would be dangling.
-        }
-
-        // Unlike the simple() function, we don't consider min_overlaps here, as there
-        // is no point having max_gap if we need to enforce min_overlaps as well.
-        const auto& current_node = index.nodes[current_index];
-
-        matches.push_back(current_node.id);
-        if (quit_on_first) {
-            return;
-        }
-        if (current_node.duplicates_start != current_node.duplicates_end) {
-            matches.insert(matches.end(), index.duplicates.begin() + current_node.duplicates_start, index.duplicates.begin() + current_node.duplicates_end);
-        }
-
-        if (current_node.children_start != current_node.children_end) {
-            Index_ start_pos = std::lower_bound(index.ends.begin() + current_node.children_start, index.ends.begin() + current_node.children_end, search_query_start) - index.ends.begin();
-            if (start_pos != current_node.children_end) {
-                workspace.history.emplace_back(current_index, start_pos);
-            }
-        }
-    }
-}
-/**
- * @endcond
- */
 
 template<typename Position_>
 struct OverlapsAnyParameters {
@@ -166,6 +32,137 @@ struct OverlapsAnyParameters {
     Position_ min_overlap = 0;
     bool quit_on_first = false;
 };
+
+/**
+ * @cond
+ */
+template<bool gapped_, typename Index_, typename Position_>
+void overlaps_any_internal(
+    const Nclist<Index_, Position_>& index,
+    Position_ query_start,
+    Position_ query_end,
+    const OverlapsAnyParameters<Position_>& params,
+    OverlapsAnyWorkspace<Index_>& workspace,
+    std::vector<Index_>& matches)
+{
+    matches.clear();
+    if (index.root_children == 0) {
+        return;
+    }
+    if constexpr(!gapped_) {
+        if (params.min_overlap > 0 && query_end - query_start < params.min_overlap) {
+            return;
+        }
+    }
+
+    auto is_finished = [&](Position_ index_start) -> bool {
+        if constexpr(!gapped_) {
+            return index_start >= query_end;
+        } else {
+            if (index_start < query_end) {
+                return false;
+            }
+            return index_start - query_end > params.max_gap; // not >=, as a gap-extended end is inclusive to the starts.
+        }
+    };
+
+    auto gapped_query_start = [&]{
+        if constexpr(!gapped_) {
+            return static_cast<char*>(NULL); // return a NULL pointer to make sure it's never used.
+        } else if (std::is_unsigned<Position_>::value && query_start < *(params.max_gap)) {
+            return static_cast<Position_>(0);
+        } else {
+            return query_start - *(params.max_gap);
+        }
+    }();
+
+    // We add a function to check if the first index end precedes the
+    // (effective) query start, and to skip the binary search if so. This is
+    // often true when traversing through nested children; if the parent
+    // skips, many of its children will also skip.
+    auto precedes_query = [&](Position_ index_end) -> bool {
+        if constexpr(!gapped_) {
+            return index_end <= query_start;
+        } else {
+            return index_end < gapped_query_start;
+        }
+    };
+
+    auto find_first_child = [&](Index_ children_start, Index_ children_end) -> Index_ {
+        auto ebegin = index.ends.begin();
+        auto estart = ebegin + children_start; 
+        auto eend = ebegin + children_end;
+        if constexpr(!gapped_) {
+            // We use an upper bound as the interval ends are assumed to be non-inclusive,
+            // so we want the first index end that is greater than the query_start.
+            return std::upper_bound(estart, eend, query_start) - ebegin;
+        } else {
+            // Now use a lower bound as the gap-subtracted start is inclusive with the interval ends.
+            // Consider a gap of zero; this means that a query that is exactly contiguous with a 
+            // subject interval will be considered as overlapping that interval.
+            return std::lower_bound(estart, eend, gapped_query_start) - ebegin;
+        }
+    };
+
+    Index_ root_child_at = 0;
+    if (precedes_query(index.ends[0])) {
+        root_child_at = find_first_child(0, index.root_children);
+    }
+
+    workspace.history.clear();
+    while (1) {
+        Index_ current_index;
+        if (workspace.history.empty()) {
+            if (root_child_at == index.root_children || is_finished(index.starts[root_child_at])) {
+                break;
+            }
+            current_index = root_child_at;
+            ++root_child_at;
+        } else {
+            auto& current_state = workspace.history.back();
+            if (current_state.child_at == current_state.child_end || is_finished(index.starts[current_state.child_at])) {
+                workspace.history.pop_back();
+                continue;
+            }
+            current_index = current_state.child_at;
+            ++(current_state.child_at); // do this before the emplace_back(), otherwise the history might get reallocated and the reference would be dangling.
+        }
+
+        const auto& current_node = index.nodes[current_index];
+        if constexpr(!gapped_) {
+            if (params.min_overlap > 0) {
+                if (std::min(query_end, index.ends[current_index]) - std::max(query_start, index.starts[current_index]) < params.min_overlap) {
+                    // No point continuing with the children, as all children will by definition have smaller overlaps and cannot satisfy 'min_overlap'.
+                    continue;
+                }
+            }
+        }
+
+        matches.push_back(current_node.id);
+        if (params.quit_on_first) {
+            return;
+        }
+        if (current_node.duplicates_start != current_node.duplicates_end) {
+            matches.insert(matches.end(), index.duplicates.begin() + current_node.duplicates_start, index.duplicates.begin() + current_node.duplicates_end);
+        }
+
+        if (current_node.children_start != current_node.children_end) {
+            if (precedes_query(index.ends[current_node.children_start])) {
+                // +1 to skip the first as we already know it's before our query_start.
+                Index_ start_pos = find_first_child(current_node.children_start + 1, current_node.children_end);
+                if (start_pos != current_node.children_end) {
+                    workspace.history.emplace_back(start_pos, current_node.children_end);
+                }
+            } else {
+                // Skip the binary search.
+                workspace.history.emplace_back(current_node.children_start, current_node.children_end);
+            }
+        }
+    }
+}
+/**
+ * @endcond
+ */
 
 template<typename Index_, typename Position_>
 void overlaps_any(
@@ -177,9 +174,9 @@ void overlaps_any(
     std::vector<Index_>& matches)
 {
     if (params.max_gap.has_value()) {
-        overlaps_any_gapped(index, query_start, query_end, *(params.max_gap), params.quit_on_first, workspace, matches);
+        overlaps_any_internal<true>(index, query_start, query_end, params, workspace, matches);
     } else {
-        overlaps_any_simple(index, query_start, query_end, params.min_overlap, params.quit_on_first, workspace, matches);
+        overlaps_any_internal<false>(index, query_start, query_end, params, workspace, matches);
     }
 }
 
--- tests/src/overlaps_any.cpp
+++ tests/src/overlaps_any.cpp
@@ -14,6 +14,8 @@ TEST(OverlapsAny, Empty) {
     EXPECT_TRUE(output.empty());
 }
 
+/********************************************************************/
+
 TEST(OverlapsAny, SimpleDisjoint) {
     std::vector<int> test_starts { 200, 300, 100, 500 };
     std::vector<int> test_ends { 280, 320, 170, 510 };
@@ -21,6 +23,11 @@ TEST(OverlapsAny, SimpleDisjoint) {
     auto index = nclist::build<int, int>(test_starts.size(), test_starts.data(), test_ends.data());
     nclist::OverlapsAnyWorkspace<int> workspace;
     std::vector<int> output;
+
+    {
+        nclist::overlaps_any(index, 50, 80, nclist::OverlapsAnyParameters<int>(), workspace, output);
+        EXPECT_TRUE(output.empty());
+    }
 
     {
         nclist::overlaps_any(index, 150, 200, nclist::OverlapsAnyParameters<int>(), workspace, output);
@@ -52,6 +59,98 @@ TEST(OverlapsAny, SimpleDisjoint) {
         EXPECT_EQ(output[1], 1);
         EXPECT_EQ(output[2], 2);
         EXPECT_EQ(output[3], 3);
+    }
+}
+
+TEST(OverlapsAny, SimpleMaxGap) {
+    std::vector<int> test_starts { 200, 300, 100, 500 };
+    std::vector<int> test_ends { 280, 320, 170, 510 };
+
+    auto index = nclist::build<int, int>(test_starts.size(), test_starts.data(), test_ends.data());
+    nclist::OverlapsAnyWorkspace<int> workspace;
+    std::vector<int> output, default_output;
+
+    nclist::OverlapsAnyParameters<int> default_params;
+    nclist::OverlapsAnyParameters<int> params;
+    params.max_gap = 0;
+
+    {
+        nclist::overlaps_any(index, 90, 100, default_params, workspace, default_output);
+        EXPECT_TRUE(default_output.empty());
+        nclist::overlaps_any(index, 90, 100, params, workspace, output);
+        ASSERT_EQ(output.size(), 1);
+        EXPECT_EQ(output[0], 2);
+    }
+
+    {
+        nclist::overlaps_any(index, 510, 520, default_params, workspace, default_output);
+        EXPECT_TRUE(default_output.empty());
+        nclist::overlaps_any(index, 510, 520, params, workspace, output);
+        ASSERT_EQ(output.size(), 1);
+        EXPECT_EQ(output[0], 3);
+    }
+
+    // Trying with a bigger max gap.
+    params.max_gap = 10;
+    {
+        nclist::overlaps_any(index, 290, 290, default_params, workspace, default_output);
+        EXPECT_TRUE(default_output.empty());
+        nclist::overlaps_any(index, 290, 290, params, workspace, output);
+        ASSERT_EQ(output.size(), 2);
+        std::sort(output.begin(), output.end());
+        EXPECT_EQ(output[0], 0);
+        EXPECT_EQ(output[1], 1);
+    }
+
+    params.max_gap = 100;
+    {
+        nclist::overlaps_any(index, 300, 400, default_params, workspace, default_output);
+        ASSERT_EQ(default_output.size(), 1);
+        EXPECT_EQ(default_output[0], 1);
+        nclist::overlaps_any(index, 300, 400, params, workspace, output);
+        ASSERT_EQ(output.size(), 3);
+        std::sort(output.begin(), output.end());
+        EXPECT_EQ(output[0], 0);
+        EXPECT_EQ(output[1], 1);
+        EXPECT_EQ(output[2], 3);
+    }
+}
+
+TEST(OverlapsAny, SimpleMinOverlap) {
+    std::vector<int> test_starts { 200, 300, 100, 500 };
+    std::vector<int> test_ends { 280, 320, 170, 510 };
+
+    auto index = nclist::build<int, int>(test_starts.size(), test_starts.data(), test_ends.data());
+    nclist::OverlapsAnyWorkspace<int> workspace;
+    std::vector<int> output, default_output;
+
+    nclist::OverlapsAnyParameters<int> default_params;
+    nclist::OverlapsAnyParameters<int> params;
+    params.min_overlap = 10;
+
+    {
+        nclist::overlaps_any(index, 100, 105, default_params, workspace, default_output);
+        ASSERT_EQ(default_output.size(), 1);
+        EXPECT_EQ(default_output[0], 2);
+        nclist::overlaps_any(index, 100, 105, params, workspace, output);
+        EXPECT_TRUE(output.empty());
+    }
+
+    {
+        nclist::overlaps_any(index, 275, 310, default_params, workspace, default_output);
+        ASSERT_EQ(default_output.size(), 2);
+        std::sort(default_output.begin(), default_output.end());
+        EXPECT_EQ(default_output[0], 0);
+        EXPECT_EQ(default_output[1], 1);
+        nclist::overlaps_any(index, 275, 310, params, workspace, output);
+        ASSERT_EQ(output.size(), 1);
+        EXPECT_EQ(output[0], 1);
+    }
+
+    // Skipping if the input range is too small.
+    {
+        nclist::overlaps_any(index, 290, 295, params, workspace, output);
+        EXPECT_TRUE(output.empty());
     }
 }
 
@@ -136,14 +235,14 @@ TEST(OverlapsAny, SimpleNested) {
     }
 }
 
-class OverlapsAnyRandomizedTest : public ::testing::TestWithParam<std::tuple<int, int> > {
+class OverlapsAnyTestCore {
 protected:
     int nquery, nsubject;
     std::vector<int> query_start, query_end;
     std::vector<int> subject_start, subject_end;
 
-    void SetUp() {
-        auto params = GetParam();
+    template<typename Parameters_>
+    void assemble(const Parameters_& params) {
         nquery = std::get<0>(params);
         nsubject = std::get<1>(params);
         std::mt19937_64 rng(nquery * 13 + nsubject);
@@ -168,15 +267,24 @@ protected:
     }
 };
 
-TEST_P(OverlapsAnyRandomizedTest, Basic) {
+class OverlapsAnyReferenceTest : public OverlapsAnyTestCore, public ::testing::TestWithParam<std::tuple<int, int> > {
+protected:
+    void SetUp() {
+        assemble(GetParam());
+    }
+};
+
+TEST_P(OverlapsAnyReferenceTest, Basic) {
     std::vector<std::vector<int> > ref; 
     reference_search(query_start, query_end, subject_start, subject_end, ref);
 
     auto index = nclist::build(nsubject, subject_start.data(), subject_end.data());
-    std::vector<int> results;
     nclist::OverlapsAnyWorkspace<int> work;
+    nclist::OverlapsAnyParameters<int> params;
+    std::vector<int> results;
+
     for (int q = 0; q < nquery; ++q) {
-        nclist::overlaps_any(index, query_start[q], query_end[q], nclist::OverlapsAnyParameters<int>(), work, results);
+        nclist::overlaps_any(index, query_start[q], query_end[q], params, work, results);
         std::sort(results.begin(), results.end());
         EXPECT_EQ(results, ref[q]);
     }
@@ -184,9 +292,91 @@ TEST_P(OverlapsAnyRandomizedTest, Basic) {
 
 INSTANTIATE_TEST_SUITE_P(
     OverlapsAny,
-    OverlapsAnyRandomizedTest,
+    OverlapsAnyReferenceTest,
     ::testing::Combine(
-        ::testing::Values(10, 100, 1000),
-        ::testing::Values(10, 100, 1000)
+        ::testing::Values(10, 100, 1000), // num of query ranges
+        ::testing::Values(10, 100, 1000) // number of subject ranges
     )
 );
+
+class OverlapsAnyMinOverlapTest : public OverlapsAnyTestCore, public ::testing::TestWithParam<std::tuple<int, int, int> > {
+protected:
+    int min_overlap;
+    void SetUp() {
+        auto params = GetParam();
+        assemble(params);
+        min_overlap = std::get<2>(params);
+    }
+};
+
+TEST_P(OverlapsAnyMinOverlapTest, Basic) {
+    auto index = nclist::build(nsubject, subject_start.data(), subject_end.data());
+    nclist::OverlapsAnyWorkspace<int> work;
+    nclist::OverlapsAnyParameters<int> default_params, params;
+    params.min_overlap = min_overlap;
+    std::vector<int> results, filtered;
+
+    for (int q = 0; q < nquery; ++q) {
+        nclist::overlaps_any(index, query_start[q], query_end[q], default_params, work, results);
+        filtered.clear();
+        for (auto r : results) {
+            if (min_overlap <= std::min(query_end[q], subject_end[r]) - std::max(query_start[q], subject_start[r])) {
+                filtered.push_back(r);
+            }
+        }
+        std::sort(filtered.begin(), filtered.end());
+
+        nclist::overlaps_any(index, query_start[q], query_end[q], params, work, results);
+        std::sort(results.begin(), results.end());
+        EXPECT_EQ(results, filtered);
+    }
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    OverlapsAny,
+    OverlapsAnyMinOverlapTest,
+    ::testing::Combine(
+        ::testing::Values(10, 100, 1000), // num of query ranges
+        ::testing::Values(10, 100, 1000), // number of subject ranges
+        ::testing::Values(5, 20) // minimum overlap 
+    )
+);
+
+class OverlapsAnyMaxGapTest : public OverlapsAnyTestCore, public ::testing::TestWithParam<std::tuple<int, int, int> > {
+protected:
+    int max_gap;
+    void SetUp() {
+        auto params = GetParam();
+        assemble(params);
+        max_gap = std::get<2>(params);
+    }
+};
+
+TEST_P(OverlapsAnyMaxGapTest, Basic) {
+    auto index = nclist::build(nsubject, subject_start.data(), subject_end.data());
+    nclist::OverlapsAnyWorkspace<int> work;
+    nclist::OverlapsAnyParameters<int> default_params, params;
+    params.max_gap = max_gap;
+    std::vector<int> results, default_results;
+
+    for (int q = 0; q < nquery; ++q) {
+        // Pushing it out by an extra 1 so that the query start/end boundaries are now inclusive when comparing to subject end/start coordinates, respectively.
+        nclist::overlaps_any(index, query_start[q] - max_gap - 1, query_end[q] + max_gap + 1, default_params, work, default_results);
+        std::sort(default_results.begin(), default_results.end());
+
+        nclist::overlaps_any(index, query_start[q], query_end[q], params, work, results);
+        std::sort(results.begin(), results.end());
+        EXPECT_EQ(results, default_results);
+    }
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    OverlapsAny,
+    OverlapsAnyMaxGapTest,
+    ::testing::Combine(
+        ::testing::Values(10, 100, 1000), // num of query ranges
+        ::testing::Values(10, 100, 1000), // number of subject ranges
+        ::testing::Values(0, 10) // maximum gap
+    )
+);
+
